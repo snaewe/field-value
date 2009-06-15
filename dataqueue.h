a47112331a6f8d3bd1ab04eaf0517827f94780e9
Stefan Naewe <stefan.naewe@atlas-elektronik.com>
2009-06-15 13:17:14 +0200

    made DataQueue a little more capable
    Signed-off-by: Stefan Naewe <stefan.naewe@atlas-elektronik.com>

--- dataqueue.h
+++ dataqueue.h
@@ -6,10 +6,23 @@ class DataQueue
 {
 public:
     typedef long value_type;
+
+    DataQueue()
+    :value()
+    {
+    }
+    
+    DataQueue(value_type initialValue)
+    :value(initialValue)
+    {
+    }
     
     bool getAnyValue(value_type& val)
     {
+        val = value;
         return true;
     }
+private:
+    value_type value;
 };
 #endif
